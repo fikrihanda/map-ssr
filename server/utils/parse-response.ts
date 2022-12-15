62c4c01b5c5cf982c6dca99eaa4920b77ed26b0c
Fikri Handa <fikrihanda@gmail.com>
2022-12-15 14:54:16 +0700

    cuma test type

--- server/utils/parse-response.ts
+++ server/utils/parse-response.ts
@@ -5,9 +5,9 @@ export const parseData = function<R = any> (stringBody: string, keys: (keyof R)[
     for (const [i, cur] of curSplit.entries()) {
       const key = keys[i]
       if (key === 'lat' || key === 'lng')
-        re[key] = parseFloat(cur) as any
+        re[key] = parseFloat(cur) as R[keyof R]
       else
-        re[key] = cur as any
+        re[key] = cur as R[keyof R]
     }
     prev.push(re)
     return prev
